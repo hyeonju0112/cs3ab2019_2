a5b562e4d3a9dee7a53d962dedc8e6f13aa2e30c
donginham <hdihdi1869@naver.com>
2019-06-12 17:16:28 +0900

    20190612 commit

---
+++ src/main/java/iducs/springboot/board/utils/HttpSessionUtils.java
@@ -0,0 +1,24 @@
+package iducs.springboot.board.utils;
+
+import javax.servlet.http.HttpSession;
+
+import iducs.springboot.board.domain.User;
+
+public class HttpSessionUtils {
+	public static boolean isEmpty(HttpSession session, String key) {
+		Object object = session.getAttribute(key);
+		if(object == null) {
+			return true; //true -> 로그인 안된 상태
+		}
+		return false;
+	}
+	public static boolean isSameUser(Object login, User writer) {
+		if(login.equals(writer)) return true;
+	/*
+	 * if(login == writer){
+	 * 	return true; //동일한 객체인가?
+	 * }
+	 */
+		return false;
+	}
+}
